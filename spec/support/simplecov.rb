2226107d36d8b6d180113d57654a68bb3512b7b9
David Freerksen <dfreerksen@gmail.com>
2016-03-13 12:22:23 -0700

    Move Simplecov to project

--- spec/support/simplecov.rb
+++ spec/support/simplecov.rb
@@ -4,6 +4,7 @@ SimpleCov.start :rails do
   add_filter "dummy_generator.rb"
   add_filter "install_generator.rb"
   add_filter "version.rb"
+  add_filter "lib/archangel/testing_support"
 
   add_group "Inputs", "app/inputs"
   add_group "Modules", "app/modules"
