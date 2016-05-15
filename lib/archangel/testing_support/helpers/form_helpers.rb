2081ab11cb37d4d484cd81113999f9f912d3f42f
dfreerksen <dfreerksen@gmail.com>
2016-05-14 19:46:39 -0700

    Page slug

---
+++ lib/archangel/testing_support/helpers/form_helpers.rb
@@ -0,0 +1,13 @@
+module Archangel
+  module TestingSupport
+    module FormHelpers
+      def submit_form
+        find("input[name='commit']").click
+      end
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.include Archangel::TestingSupport::FormHelpers, type: :feature
+end
