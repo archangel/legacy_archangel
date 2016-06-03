3883784164320138e3b191669a9cff8e7ea4d5eb
dfreerksen <dfreerksen@gmail.com>
2016-06-02 21:18:46 -0700

    Required fields for user registration
    Create as general user

--- app/controllers/archangel/auth_controller.rb
+++ app/controllers/archangel/auth_controller.rb
@@ -2,10 +2,18 @@ module Archangel
   class AuthController < BaseController
     helper Archangel::AuthHelper
 
+    before_filter :configure_permitted_parameters
+
     protected
 
     def theme_resolver
       "archangel/layouts/auth"
+    end
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) do |u|
+        u.permit(:email, :name, :password, :password_confirmation, :username)
+      end
     end
   end
 end
