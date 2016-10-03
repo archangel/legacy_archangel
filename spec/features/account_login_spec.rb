692c0885b0f220252f5ba96e756409ef00a5275a
David Freerksen <dfreerksen@gmail.com>
2016-10-02 19:06:13 -0700

    Fix field type for registration form

---
+++ spec/features/account_login_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.feature "Account login page", type: :feature do
+  describe "when on registration page" do
+    it "has additional form fields" do
+      visit archangel.new_user_registration_path
+
+      expect(page).to have_text "Name"
+      expect(page).to have_text "Username"
+
+      expect(page).to have_selector("input[type=text][id='user_name']")
+      expect(page).to have_selector("input[type=text][id='user_username']")
+    end
+  end
+end
