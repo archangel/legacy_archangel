2328cb551fa9c6c7ba95e1aa628145aefdc536da
David Freerksen <dfreerksen@gmail.com>
2016-10-20 22:04:36 -0700

    Test layout

--- spec/controllers/archangel/admin/dashboards_controller_spec.rb
+++ spec/controllers/archangel/admin/dashboards_controller_spec.rb
@@ -5,12 +5,6 @@ module Archangel
     RSpec.describe DashboardsController, type: :controller do
       before { stub_authorization! }
 
-      it "uses correct layout" do
-        archangel_get :show
-
-        expect(response).to render_template(layout: "archangel/layouts/admin")
-      end
-
       describe "GET #show" do
         it "assigns the requested issue as @issue" do
           archangel_get :show
