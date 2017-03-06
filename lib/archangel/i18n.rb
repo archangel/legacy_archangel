33ae7aa34ca0260b500175b34edde46e38d66d1d
David Freerksen <dfreerksen@gmail.com>
2017-03-05 19:32:53 -0800

    Document constants

--- lib/archangel/i18n.rb
+++ lib/archangel/i18n.rb
@@ -7,6 +7,27 @@ module Archangel
   extend ActionView::Helpers::TranslationHelper
 
   class << self
+    # Flash message type based on Bootstrap flash types
+    #
+    # = Example
+    #   en:
+    #     archangel:
+    #       foo: Bar
+    #       hello:
+    #         word: Hello World
+    #
+    #   "<%= Archangel.translate('archangel.foo') %>" #=> "Bar"
+    #   "<%= Archangel.translate(:foo) %>" #=> "Bar"
+    #   "<%= Archangel.translate('foo') %>" #=> "Bar"
+    #   "<%= Archangel.t('archangel.foo') %>" #=> "Bar"
+    #   "<%= Archangel.t(:foo) %>" #=> "Bar"
+    #   "<%= Archangel.t('foo') %>" #=> "Bar"
+    #   "<%= Archangel.t('hello.world') %>" #=> "Hello World"
+    #
+    # @param [String, Symbol] args
+    #                         translation argument
+    # @return [String] translated string
+    #
     def translate(*args)
       options = args.extract_options!
       options[:scope] = [*options[:scope]].unshift(:archangel)
--- lib/archangel/version.rb
+++ lib/archangel/version.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 module Archangel
+  # Archangel version
+  #
   VERSION = "0.0.1".freeze
 end
