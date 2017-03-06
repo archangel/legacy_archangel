941673daaf3da1532b2d385b3e2adc78c4e1dc50
David Freerksen <dfreerksen@gmail.com>
2017-03-05 19:59:38 -0800

    Document helper methods

--- app/helpers/archangel/admin_helper.rb
+++ app/helpers/archangel/admin_helper.rb
@@ -9,6 +9,21 @@ module Archangel
   module AdminHelper
     include FontAwesome::Rails::IconHelper
 
+    # Author link
+    #
+    # The current logged in user will link to the profile. All others will link
+    # to the User.
+    #
+    # = Example
+    #   "<%= author_link(resource.author) %>"
+    #     #=> "admin_user_path(resource.author)"
+    #   "<%= author_link(resource.author) %>"
+    #     #=> "admin_profile_path # when current user"
+    #
+    # @param [Archangel::User] author
+    #                          author resource
+    # @return [String] link to user
+    #
     def author_link(author)
       author ||= Archangel::User.new
 
@@ -21,24 +36,84 @@ module Archangel
       link_to(author.name, link)
     end
 
+    # Date picker field value
+    #
+    # Returns value in the format of `%Y-%m-%d`
+    #
+    # = Example
+    #   "<%= datepicker_field_value(resource.published_at) %>"
+    #   "<%= datepicker_field_value(resource.published_at, true) %>"
+    #   "<%= datepicker_field_value(resource.published_at, false) %>"
+    #
+    # @param [Datetime] date
+    #                   date object
+    # @param [Boolean] now
+    #                  use current time
+    # @return [String] value for time picker
+    #
     def datepicker_field_value(date, now = false)
       date_field_value(date,
                        Archangel.t(:date_format, scope: :datetimepicker),
                        now)
     end
 
+    # Time picker field value
+    #
+    # Returns value in the format of `%H:%M`
+    #
+    # = Example
+    #   "<%= timepicker_field_value(resource.published_at) %>"
+    #   "<%= timepicker_field_value(resource.published_at, true) %>"
+    #   "<%= timepicker_field_value(resource.published_at, false) %>"
+    #
+    # @param [Datetime] date
+    #                   date object
+    # @param [Boolean] now
+    #                  use current time
+    # @return [String] value for time picker
+    #
     def timepicker_field_value(date, now = false)
       date_field_value(date,
                        Archangel.t(:time_format, scope: :datetimepicker),
                        now)
     end
 
+    # Datetime picker field value
+    #
+    # Returns value in the format of `%Y-%m-%d %H:%M`
+    #
+    # = Example
+    #   "<%= datetimepicker_field_value(resource.published_at) %>"
+    #   "<%= datetimepicker_field_value(resource.published_at, true) %>"
+    #   "<%= datetimepicker_field_value(resource.published_at, false) %>"
+    #
+    # @param [Datetime] date
+    #                   date object
+    # @param [Boolean] now
+    #                  use current time
+    # @return [String] value for datetime picker
+    #
     def datetimepicker_field_value(date, now = false)
       date_field_value(date,
                        Archangel.t(:format, scope: :datetimepicker),
                        now)
     end
 
+    # Date field value
+    #
+    # = Example
+    #   "<%= date_field_value(resource.published_at, '%y%m%d') %>"
+    #   "<%= date_field_value(resource.published_at, '%y%m%d', true) %>"
+    #   "<%= date_field_value(resource.published_at, '%y%m%d', false) %>"
+    #
+    # @param [Datetime] date
+    #                   date object
+    # @param [String] format
+    #                 date format
+    # @param [Boolean] now
+    #                  use current time
+    # @return [String] value for date picker
+    #
     def date_field_value(date, format, now = false)
       date ||= Time.current if now
 
