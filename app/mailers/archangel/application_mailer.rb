7f922da8e711864af28ede15f33023b4beddb606
David Freerksen <dfreerksen@gmail.com>
2017-03-05 19:32:26 -0800

    Document mailers

--- app/mailers/archangel/application_mailer.rb
+++ app/mailers/archangel/application_mailer.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 module Archangel
+  # Application mailer
+  #
+  # @author dfreerksen
+  # @since 0.0.1
+  #
   class ApplicationMailer < ActionMailer::Base
     default from: "noreply@example.com"
+
     layout "mailer"
   end
 end
