a099f8d8df871cefbbcc35cd805d1586b88b9b6a
David Freerksen <dfreerksen@gmail.com>
2017-04-01 15:29:49 -0700

    Admin users documentation

--- app/controllers/archangel/admin/users_controller.rb
+++ app/controllers/archangel/admin/users_controller.rb
@@ -16,6 +16,10 @@ module Archangel
 
       # List of all users
       #
+      # Current logged in user is not returned in this list. You are not able to
+      # view or modify yourself in the same way you are able to view and modify
+      # others
+      #
       # = Request
       #   GET /admin/users
       #   GET /admin/users/page/:page
@@ -29,10 +33,39 @@ module Archangel
       # = Response
       #   [
       #     {
-      #       "id": 123,
+      #       "id": 2,
       #       "name": "John Doe",
-      #       "username": "john-doe"
-      #     }
+      #       "username": "john-doe",
+      #       "role": "admin",
+      #       "avatar": {
+      #         "url": "/path/to/avatar.jpg",
+      #         "medium": {
+      #           "url": "/path/to/medium_avatar.jpg"
+      #         },
+      #         "thumb": {
+      #           "url": "/path/to/thumb_avatar.jpg"
+      #         },
+      #        "mini": {
+      #           "url": "/path/to/mini_avatar.jpg"
+      #         },
+      #         "micro": {
+      #           "url": "/path/to/micro_avatar.jpg"
+      #         }
+      #       },
+      #       "email": "user@example.com",
+      #       "invitation_token": "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+      #       "invitation_created_at": "YYYY-MM-DDTHH:MM:SS.MSZ",
+      #       "invitation_sent_at": "YYYY-MM-DDTHH:MM:SS.MSZ",
+      #       "invitation_accepted_at": "YYYY-MM-DDTHH:MM:SS.MSZ",
+      #       "invitation_limit": null,
+      #       "invited_by_type": "Archangel::User",
+      #       "invited_by_id": 123,
+      #       "invitations_count": 0,
+      #       "deleted_at": null,
+      #       "created_at": "YYYY-MM-DDTHH:MM:SS.MSZ",
+      #       "updated_at": "YYYY-MM-DDTHH:MM:SS.MSZ"
+      #     },
+      #     ...
       #   ]
       #
       def index
@@ -42,19 +75,47 @@ module Archangel
       # View a user
       #
       # = Request
-      #   GET /admin/users/:id
+      #   GET /admin/user/:username
       #
       # = Formats
       #   HTML, JSON
       #
       # = Params
-      #   [Integer] id - the user ID
+      #   [Integer] username - the user username
       #
       # = Response
       #   {
-      #     "id": 123,
+      #     "id": 2,
       #     "name": "John Doe",
-      #     "username": "john-doe"
+      #     "username": "john-doe",
+      #     "role": "admin",
+      #     "avatar": {
+      #       "url": "/path/to/avatar.jpg",
+      #       "medium": {
+      #         "url": "/path/to/medium_avatar.jpg"
+      #       },
+      #       "thumb": {
+      #         "url": "/path/to/thumb_avatar.jpg"
+      #       },
+      #      "mini": {
+      #         "url": "/path/to/mini_avatar.jpg"
+      #       },
+      #       "micro": {
+      #         "url": "/path/to/micro_avatar.jpg"
+      #       }
+      #     },
+      #     "email": "user@example.com",
+      #     "invitation_token": "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+      #     "invitation_created_at": "YYYY-MM-DDTHH:MM:SS.MSZ",
+      #     "invitation_sent_at": "YYYY-MM-DDTHH:MM:SS.MSZ",
+      #     "invitation_accepted_at": "YYYY-MM-DDTHH:MM:SS.MSZ",
+      #     "invitation_limit": null,
+      #     "invited_by_type": "Archangel::User",
+      #     "invited_by_id": 123,
+      #     "invitations_count": 0,
+      #     "deleted_at": null,
+      #     "created_at": "YYYY-MM-DDTHH:MM:SS.MSZ",
+      #     "updated_at": "YYYY-MM-DDTHH:MM:SS.MSZ"
       #   }
       #
       def show
@@ -71,9 +132,37 @@ module Archangel
       #
       # = Response
       #   {
-      #     "id": 0,
+      #     "id": null,
       #     "name": "",
-      #     "username": ""
+      #     "username": "",
+      #     "role": "editor",
+      #     "avatar": {
+      #       "url": "/path/to/default_avatar.jpg",
+      #       "medium": {
+      #         "url": "/path/to/medium_default_avatar.jpg",
+      #       },
+      #       "thumb": {
+      #         "url": "/path/to/thumb_default_avatar.jpg",
+      #       },
+      #       "mini": {
+      #         "url": "/path/to/mini_default_avatar.jpg",
+      #       },
+      #       "micro": {
+      #         "url": "/path/to/micro_default_avatar.jpg",
+      #       }
+      #     },
+      #     "email": "",
+      #     "invitation_token": null,
+      #     "invitation_created_at": null,
+      #     "invitation_sent_at": null,
+      #     "invitation_accepted_at": null,
+      #     "invitation_limit": null,
+      #     "invited_by_type": null,
+      #     "invited_by_id": null,
+      #     "invitations_count": 0,
+      #     "deleted_at": null,
+      #     "created_at": null,
+      #     "updated_at": null
       #   }
       #
       def new
@@ -91,16 +180,12 @@ module Archangel
       # = Request
       #   {
       #     "user": {
-      #       "name": "John Doe",
-      #       "username": "john-doe"
+      #       "name": "New User",
+      #       "username": "new-user",
+      #       "role": "editor",
+      #       "avatar": "local/path/to/avatar.jpg",
+      #       "email": "user@example.com"
       #     }
-      #   }
-      #
-      # = Response
-      #   {
-      #     "id": 123,
-      #     "name": "John Doe",
-      #     "username": "john-doe"
       #   }
       #
       def create
@@ -112,16 +197,47 @@ module Archangel
       # Edit a user
       #
       # = Request
-      #   GET /admin/users/:id/edit
+      #   GET /admin/users/:username/edit
       #
       # = Formats
       #   HTML, JSON
       #
+      # = Params
+      #   [Integer] username - the user username
+      #
       # = Response
       #   {
-      #     "id": 123,
+      #     "id": 2,
       #     "name": "John Doe",
-      #     "slug": "john-doe"
+      #     "username": "john-doe",
+      #     "role": "admin",
+      #     "avatar": {
+      #       "url": "/path/to/avatar.jpg",
+      #       "medium": {
+      #         "url": "/path/to/medium_avatar.jpg"
+      #       },
+      #       "thumb": {
+      #         "url": "/path/to/thumb_avatar.jpg"
+      #       },
+      #      "mini": {
+      #         "url": "/path/to/mini_avatar.jpg"
+      #       },
+      #       "micro": {
+      #         "url": "/path/to/micro_avatar.jpg"
+      #       }
+      #     },
+      #     "email": "user@example.com",
+      #     "invitation_token": "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+      #     "invitation_created_at": "YYYY-MM-DDTHH:MM:SS.MSZ",
+      #     "invitation_sent_at": "YYYY-MM-DDTHH:MM:SS.MSZ",
+      #     "invitation_accepted_at": "YYYY-MM-DDTHH:MM:SS.MSZ",
+      #     "invitation_limit": null,
+      #     "invited_by_type": "Archangel::User",
+      #     "invited_by_id": 123,
+      #     "invitations_count": 0,
+      #     "deleted_at": null,
+      #     "created_at": "YYYY-MM-DDTHH:MM:SS.MSZ",
+      #     "updated_at": "YYYY-MM-DDTHH:MM:SS.MSZ"
       #   }
       #
       def edit
@@ -131,25 +247,24 @@ module Archangel
       # Update a user
       #
       # = Request
-      #   PATCH /admin/users/:id
-      #   PUT   /admin/users/:id
+      #   PATCH /admin/users/:username
+      #   PUT   /admin/users/:username
       #
       # = Formats
       #   HTML, JSON
       #
+      # = Params
+      #   [Integer] username - the user username
+      #
       # = Request
       #   {
       #     "user": {
-      #       "name": "John Doe",
-      #       "username": "john-doe"
+      #       "name": "Updated Name",
+      #       "username": "updated-name",
+      #       "role": "editor",
+      #       "avatar": "local/path/to/avatar.jpg",
+      #       "email": "user@example.com"
       #     }
-      #   }
-      #
-      # = Response
-      #   {
-      #     "id": 123,
-      #     "name": "John Doe",
-      #     "username": "john-doe"
       #   }
       #
       def update
@@ -160,17 +275,18 @@ module Archangel
 
       # Destroy a user
       #
-      # This does not destroy the record. This only marks the user as
-      # deleted
+      # This does not destroy the record. This only marks the user as deleted.
+      # Email address and username are prefixed with current timestamp so they
+      # can be used again
       #
       # = Request
-      #   DELETE /admin/users/:id
+      #   DELETE /admin/users/:username
       #
       # = Formats
       #   HTML, JSON
       #
       # = Params
-      #   [Integer] id - the user ID
+      #   [Integer] username - the user username
       #
       def destroy
         @user.destroy
@@ -219,3 +335,66 @@ module Archangel
     end
   end
 end
+
+
+
+
+      # # Create a new page
+      # #
+      # # = Request
+      # #   POST /admin/pages
+      # #
+      # # = Formats
+      # #   HTML, JSON
+      # #
+      # # = Request
+      # #   {
+      # #     "page": {
+      # #       "title": "Updated Page Title",
+      # #       "author_id": 123,
+      # #       "parent_id": null,
+      # #       "slug": "updated-slug",
+      # #       "homepage": false,
+      # #       "content": "</p>Updated content of the page</p>",
+      # #       "meta_keywords": "updated, keywords, for, the, page",
+      # #       "meta_description": "Updated description of the page",
+      # #       "published_at": "YYYY-MM-DDTHH:MM:SS.MSZ",
+      # #     }
+      # #   }
+      # #
+      # def create
+      #   @page.save
+      #
+      #   respond_with @page, location: -> { admin_pages_path }
+      # end
+      #
+      # # Edit a page
+      # #
+      # # = Request
+      # #   GET /admin/pages/:id/edit
+      # #
+      # # = Formats
+      # #   HTML, JSON
+      # #
+      # # = Response
+      # #   {
+      # #     "id": 123,
+      # #     "title": "Page Title",
+      # #     "author_id": 123,
+      # #     "parent_id": null,
+      # #     "path": "foo/bar",
+      # #     "slug": "bar",
+      # #     "homepage": false,
+      # #     "content": "</p>Content of the page</p>",
+      # #     "meta_keywords": "keywords, for, the, page",
+      # #     "meta_description": "Description of the page",
+      # #     "deleted_at": null,
+      # #     "published_at": "YYYY-MM-DDTHH:MM:SS.MSZ",
+      # #     "created_at": "YYYY-MM-DDTHH:MM:SS.MSZ",
+      # #     "updated_at": "YYYY-MM-DDTHH:MM:SS.MSZ"
+      # #   }
+      # #
+      # def edit
+      #   respond_with @page
+      # end
+      #
