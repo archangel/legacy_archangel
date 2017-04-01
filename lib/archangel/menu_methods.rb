ac7ba71b98cc3d79cde90f1f95b82ff3a94e279f
David Freerksen <dfreerksen@gmail.com>
2017-04-01 16:18:27 -0700

    Menu constant documentation

--- lib/archangel/menu_methods.rb
+++ lib/archangel/menu_methods.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 module Archangel
+  # Available menu item methods
+  #
   MENU_METHODS = %w(DELETE GET POST PUT).freeze
+
+  # Default menu item method
+  #
   MENU_METHOD_DEFAULT = nil
 end
