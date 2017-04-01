0ec6c2f668695de7886c900c627527f8087911cc
David Freerksen <dfreerksen@gmail.com>
2017-04-01 16:53:00 -0700

    Frontend pages documentation

--- app/controllers/archangel/frontend/pages_controller.rb
+++ app/controllers/archangel/frontend/pages_controller.rb
@@ -12,6 +12,72 @@ module Archangel
 
       helper Archangel::Frontend::PagesHelper
 
+      # View a page
+      #
+      # = Request
+      #   GET /:path
+      #
+      # = Formats
+      #   HTML, JSON
+      #
+      # = Params
+      #   [String] path - the page path
+      #
+      # = Response
+      #   # Home page (301 redirects to / if the path is requested)
+      #   {
+      #     "id": 123,
+      #     "title": "Home Page Title",
+      #     "author_id": 123,
+      #     "parent_id": null,
+      #     "path": "home-page",
+      #     "slug": "home-page",
+      #     "homepage": true,
+      #     "content": "<p>Home page content.</p>",
+      #     "meta_keywords": "keywords, for, the, page",
+      #     "meta_description": "Description of the page",
+      #     "deleted_at": null,
+      #     "published_at": "YYYY-MM-DDTHH:MM:SS.MSZ",
+      #     "created_at": "YYYY-MM-DDTHH:MM:SS.MSZ",
+      #     "updated_at": "YYYY-MM-DDTHH:MM:SS.MSZ"
+      #   }
+      #
+      #   # Root level page
+      #   {
+      #     "id": 123,
+      #     "title": "Page Title",
+      #     "author_id": 123,
+      #     "parent_id": null,
+      #     "path": "page-path",
+      #     "slug": "page-path",
+      #     "homepage": false,
+      #     "content": "<p>Page content.</p>",
+      #     "meta_keywords": "keywords, for, the, page",
+      #     "meta_description": "Description of the page",
+      #     "deleted_at": null,
+      #     "published_at": "YYYY-MM-DDTHH:MM:SS.MSZ",
+      #     "created_at": "YYYY-MM-DDTHH:MM:SS.MSZ",
+      #     "updated_at": "YYYY-MM-DDTHH:MM:SS.MSZ"
+      #   }
+      #
+      #   # Nested page
+      #   {
+      #     "id": 123,
+      #     "title": "Page Title",
+      #     "author_id": 123,
+      #     "parent_id": 123,
+      #     "path": "parent-path/page-path",
+      #     "slug": "page-path",
+      #     "homepage": false,
+      #     "content": "<p>Page content.</p>",
+      #     "meta_keywords": "keywords, for, the, page",
+      #     "meta_description": "Description of the page",
+      #     "deleted_at": null,
+      #     "published_at": "YYYY-MM-DDTHH:MM:SS.MSZ",
+      #     "created_at": "YYYY-MM-DDTHH:MM:SS.MSZ",
+      #     "updated_at": "YYYY-MM-DDTHH:MM:SS.MSZ"
+      #   }
+      #
       def show
         if redirect_to_homepage?
           return redirect_to root_path, status: :moved_permanently
