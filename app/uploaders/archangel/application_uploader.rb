c1779e90bcde1a6d34c3bb00ecf5e0c9d9d7032e
David Freerksen <dfreerksen@gmail.com>
2017-04-01 17:03:22 -0700

    Application uploader documentation

--- app/uploaders/archangel/application_uploader.rb
+++ app/uploaders/archangel/application_uploader.rb
@@ -9,22 +9,40 @@ module Archangel
   class ApplicationUploader < CarrierWave::Uploader::Base
     include CarrierWave::MiniMagick
 
+    # Path for file uploads
+    #
+    # @return [String] upload path
+    #
     def store_dir
       "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
     end
 
+    # URL for file
+    #
+    # @return [String] file url
+    #
     def default_url
       ActionController::Base.helpers.asset_path(default_path)
     end
 
+    # File extension whitelist
+    #
+    # @return [Array] file extensions
+    #
     def extension_whitelist
       Archangel.config.image_white_list
     end
 
+    # Remove animation if file is a gif based on mime type
+    #
     def remove_animation
       manipulate!(&:collapse!) if content_type == "image/gif"
     end
 
+    # Check if file is an image based on mime type
+    #
+    # @return [Boolean] if file
+    #
     def image?
       image_formats.include?(file.content_type)
     end
