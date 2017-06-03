8ebf5fe437f261fb558077f3cc4bda4f6c0f7db4
David Freerksen <dfreerksen@gmail.com>
2017-06-03 10:08:54 -0700

    Module comment

--- app/controllers/concerns/archangel/themable_concern.rb
+++ app/controllers/concerns/archangel/themable_concern.rb
@@ -1,9 +1,19 @@
 # frozen_string_literal: true
 
 module Archangel
+  # Themable concern
+  #
+  # @author dfreerksen
+  # @since 0.0.1
+  #
   module ThemableConcern
     extend ActiveSupport::Concern
 
+    # Themable concern class methods
+    #
+    # @author dfreerksen
+    # @since 0.0.1
+    #
     module ClassMethods
       def theme(theme, options = {})
         @_theme = theme
