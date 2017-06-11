350c84b7858f7606250f0d1662323353b4acf2c1
David Freerksen <dfreerksen@gmail.com>
2017-06-11 11:17:54 -0700

    Skip git in dummy generator

--- lib/generators/archangel/dummy/dummy_generator.rb
+++ lib/generators/archangel/dummy/dummy_generator.rb
@@ -41,6 +41,7 @@ module Archangel
         opts[:database] = "sqlite3" if opts[:database].blank?
         opts[:force] = true
         opts[:skip_bundle] = true
+        opts[:skip_git] = true
         opts[:old_style_hash] = false
         opts[:skip_turbolinks] = true
 
